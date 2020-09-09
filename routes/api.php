a32d622589ef0a357607422450a2d3368b92f0dd
Alavi <alaviserafin@gmail.com>
2020-09-08 22:44:41 -0400

    autentificacion

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('categorias', 'CategoriaController');
+
 Route::resource('productos', 'ProductoController');
 Route::resource('ventas', 'VentaController');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::resource('categorias', 'CategoriaController');
+});
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+Route::post('logout', 'UserController@logout');
